1f8f0797ddd6a96fff52520c03b0d453c53b14a4
Gavin Nelson <gavin@Gavins-Work-MacBook-Pro.local>
2020-03-05 11:19:55 -0800

    slight modification of text

--- scripts/scrollbehavior.js
+++ scripts/scrollbehavior.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links
 
-var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
-var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
-var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
-var is_safari = navigator.userAgent.indexOf("Safari") > -1;
-var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
-if ((is_chrome)&&(is_safari)) { is_safari = false; }
-if ((is_chrome)&&(is_opera)) { is_chrome = false; }
-
+  var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+  var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+  var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+  var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+  var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+  if ((is_chrome)&&(is_safari)) { is_safari = false; }
+  if ((is_chrome)&&(is_opera)) { is_chrome = false; }
 
   if (is_safari) {
     $("a").on('click', function(event) {
